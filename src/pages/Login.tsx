2d2bcffa9c0bfbe3157fc1cedd15638722eb4d68
Tracey Roberts <traceynicole71@yahoo.com>
2026-05-01 13:18:05 -0400

    login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,36 @@
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { AnimatedPage } from '../components/AnimatedPage';
+
+const Login = () => {
+    const { login, isAuthenticated } = useAuth();
+    const nagivate = useNavigate();
+
+    const handleLogin = () => {
+        login();
+        //redirect to admin after login
+        navigate('/admin');
+    };
+
+    return (
+<AnimatedPage>
+    <div className="flex flex-col items-center justify-center mt-20">
+        <div className="bg-white p-8 rounded-xl shadow-xl border-t-4 border-emerald-500 w-full max-w-md">
+            <h1 className="text-3xl font-bold text-slate-800 mb-6 text-center">Admin Login</h1>
+{isAuthenticated ? (
+    <p className="text-emerald-600 text-center mb-4">You are already logged in!</p>
+) : (
+    <button
+    onClick={handleLogin}
+    className="w-full bg-rose-500 hover:bg-rose-600 text-white font-bold py-3 px-4 rounded-lg transition-colors shadow-lg"
+    >
+        Log Into Dashboard
+    </button>
+)}
+        </div>
+    </div>
+</AnimatedPage>    
+);
+};
+
+export default Login;
